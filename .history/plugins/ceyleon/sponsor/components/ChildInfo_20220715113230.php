9ba104834c43a3c7d82549eac9eeef3c58e276af
Thulana Vinnath <76616997+Thulaa123@users.noreply.github.com>
2022-07-15 11:51:31 +0530

    UI Bug Fix

---
+++ .history/plugins/ceyleon/sponsor/components/ChildInfo_20220715113230.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Ceyleon\Sponsor\Components;
+
+use Ceyleon\Sponsor\Models\Child;
+
+class ChildInfo extends \Cms\Classes\ComponentBase
+{
+    public function componentDetails()
+    {
+        return [
+            'name' => 'Child Info',
+            'description' => 'Displays child info'
+        ];
+    }
+
+    public function childrendetails()
+    {
+        return Child::select()->get();
+    }
+
+    public function child()
+    {
+        return Child::find($this->property('id'));
+    }
+
+    public function sums()
+    {
+        return Child::all()->sum();
+    }
+
+}
